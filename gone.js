ea8dbc7592a6d15680136938dbb5367e753a4dac
Jasmin Bissig <jasmin.bissig@gmail.com>
2020-05-28 11:32:02 +0200

    Change rooms img onclick

---
+++ gone.js
@@ -0,0 +1,19 @@
+function showRoom1() {
+  document.getElementById("raum").src = "../img/room1.jpg";
+}
+
+function showRoom2() {
+  document.getElementById("raum").src = "../img/room2.jpg";
+}
+
+function showRoom3() {
+  document.getElementById("raum").src = "../img/room3.jpg";
+}
+
+function showRoom4() {
+  document.getElementById("raum").src = "../img/room4.jpg";
+}
+
+function showRoom5() {
+  document.getElementById("raum").src = "../img/room5.jpg";
+}
